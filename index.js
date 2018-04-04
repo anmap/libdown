5b137b381a8d616ebd490d146dd70d97645b084a
An Nguyễn <timeof2010@gmail.com>
2018-04-04 17:01:47 +0300

    Install 'commander' and test out

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
 #!/usr/bin/env node
-console.log('Libdown test');
+const libdown = require('commander');
+
+libdown
+  .arguments('<url>')
+  .option('-n, --nopdf', 'No PDF generated from downloaded images')
+  .option('-u, --ultra', 'Enforce highest image quality download where applicable (WARNING: This option might take a lot of time)')
+  .option('-l, --list', 'Show list of supported libraries')
+  .action(url => {
+    console.log('Here the URL: %s', url);
+    if (libdown.nopdf) console.log('No PDF will be generated');
+    if (libdown.ultra) console.log('');
+  })
+  .parse(process.argv);
